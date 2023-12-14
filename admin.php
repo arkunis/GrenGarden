39489186aad4cd02c15be25f79e54d9d11c50fe6
arkunis <126088452+arkunis@users.noreply.github.com>
2023-12-14 16:56:00 +0100

    up

--- admin.php
+++ admin.php
@@ -318,10 +318,10 @@ if (isset($_POST['produit'])) {
                                     </thead>
                                     <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                                         <?php foreach ($users as $row) {
-                                            if(isset($_POST['supp_'.$row['Id_Client']])){
-                                                $db->suppUser(["client"=>$row['Id_Client']]);
+                                            if (isset($_POST['supp_' . $row['Id_Client']])) {
+                                                $db->suppUser(["client" => $row['Id_Client']]);
                                             }
-                                            ?>
+                                        ?>
                                             <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
                                                 <td class="px-4 py-3">
                                                     <div class="flex items-center text-sm">
@@ -341,14 +341,51 @@ if (isset($_POST['produit'])) {
                                                 </td>
                                                 <td class="px-4 py-3 text-sm"><?= $row['Num_Client']; ?></td>
                                                 <td class="px-4 py-3 text-sm flex flex-row">
+
+                                                    <button type="btn" class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300" data-modal-target="produit_<?= $row['Id_Client']; ?>" data-modal-toggle="produit_<?= $row['Id_Client']; ?>">Modifier</button>
+
                                                     <form method="post">
-                                                        <button type="submit" name="modif_<?= $row['Id_Client']; ?>" class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Modifier</button>
-                                                    </form>
-                                                    <form method="post">
-                                                    <button type="submit" name="supp_<?= $row['Id_Client']; ?>" class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Supprimer</button>
+                                                        <button type="submit" name="supp_<?= $row['Id_Client']; ?>" class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">Supprimer</button>
                                                     </form>
                                                 </td>
                                             </tr>
+                                            <!-- Main modal -->
+                                            <div id="produit_<?= $row['Id_Client']; ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                                                    <!-- Modal content -->
+                                                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                        <!-- Modal header -->
+                                                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                                                Ajout de produit
+                                                            </h3>
+                                                            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="produit_<?= $row['Id_Client']; ?>">
+                                                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                                                </svg>
+                                                                <span class="sr-only">Close modal</span>
+                                                            </button>
+                                                        </div>
+                                                        <!-- Modal body -->
+                                                        <div class="p-4 md:p-5 space-y-4">
+                                                            <form class="space-y-4" method="post">
+                                                                <div>
+                                                                    <input type="nom" name="nom_<?= $row['Id_Client']; ?>" id="nom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nom long" required>
+                                                                </div>
+                                                                <div>
+                                                                    <input type="text" name="nom2_<?= $row['Id_Client']; ?>" id="nom2" placeholder="Nom court" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                                                                </div>
+                                                                <div class="flex flex-col gap-5 md:flex-row">
+                                                                    <input type="text" name="ref_<?= $row['Id_Client']; ?>" id="ref" placeholder="Référence" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+
+                                                                    <input type="text" name="photo_<?= $row['Id_Client']; ?>" id="photo" placeholder="URL photo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
+                                                                </div>
+                                                                <button type="submit" name="produit_<?= $row['Id_Client']; ?>" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ajouter</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         <?php } ?>
                                     </tbody>
                                 </table>
--- register.php
+++ register.php
@@ -72,7 +72,7 @@ if (isset($_POST['register'])) {
                         <label for="phone" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Numéro de tél (0600000000)</label>
                     </div>
                     <div class="relative z-0 w-full mb-5 group">
-                        <input type="text" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <input type="text" name="company" id="company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
                         <label for="company" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom de votre société</label>
                     </div>
                 </div>
