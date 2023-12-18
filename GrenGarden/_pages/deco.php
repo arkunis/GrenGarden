2469a544c75d070f127cb00a1fe8ebdb6f779796
arkunis <skyled.mgt@gmail.com>
2023-12-18 23:29:58 +0100

    update

--- GrenGarden/_pages/deco.php
+++ GrenGarden/_pages/deco.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
+if(isset($_SESSION['panier']) == true){
 $panier = $_SESSION['panier'];
 session_destroy();
 session_start();
 $_SESSION['panier'] = $panier;
+}else{
+session_destroy();
+}
 header('Location:../login.php');
